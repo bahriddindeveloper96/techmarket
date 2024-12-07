68f10d4e3656d9e9c3f1f3acd143a9f8108549bc
Bahriddin <bahriddin.developer96@gmail.com>
2024-12-07 19:04:13 +0500

    (content sizes)

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,7 +1,5 @@
 <template>
-  <main class="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
-    
-    
+  <main class="py-8">
     <div class="container mx-auto px-4 py-8">
       <!-- Favorites Header -->
       <div class="flex items-center justify-between mb-8">
