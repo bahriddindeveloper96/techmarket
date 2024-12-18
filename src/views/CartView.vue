240009a45462702b60ad6f44bfb2d814e58a92c7
Bahriddin <120379457+bahriddindeveloper96@users.noreply.github.com>
2024-12-18 22:49:48 +0500

    Update CartView.vue
    tokeni

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -182,7 +182,7 @@ const similarProducts = ref([])
 const loading = ref(false)
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://eragon.uz";
-const token = import.meta.env.VITE_API_BASE_token || "3|y0r5ZdazYN92gQ9VG10pYim8f6YWqTErZUH52NAo660ed509";
+const token = import.meta.env.VITE_API_BASE_token || "2|Yz4cT4P7rJJND86WgT5OhAQDG8E9k3l7NzcLaGDLb6810987";
 // Fetch similar products
 const fetchSimilarProducts = async () => {
   try {
