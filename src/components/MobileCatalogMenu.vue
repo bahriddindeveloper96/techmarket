cb3426d96c67aedbecb97c75c470c4394ab618f0
Bahriddin <120379457+bahriddindeveloper96@users.noreply.github.com>
2024-12-18 19:57:04 +0500

    Update MobileCatalogMenu.vue
    token

--- src/components/MobileCatalogMenu.vue
+++ src/components/MobileCatalogMenu.vue
@@ -155,8 +155,8 @@ const router = useRouter();
 const activeCategory = ref(null);
 const categories = ref([]);
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-const token = "1|THEsafwjzLqfqau7rGpgmXw1dHB3Iq6UiqAVb7Qjbe3de238";
+const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://eragon.uz";
+const token = import.meta.env.VITE_API_BASE_token || "3|y0r5ZdazYN92gQ9VG10pYim8f6YWqTErZUH52NAo660ed509";
 
 const emit = defineEmits(["close"]);
 
